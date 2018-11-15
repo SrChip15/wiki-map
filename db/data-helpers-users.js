8c3f4e5d89d299d1a0aea009b0a51037b8781467
froliceric0914 <wzhao.eric@gmail.com>
2018-11-15 23:45:19 +0000

    Create User

--- db/data-helpers-users.js
+++ db/data-helpers-users.js
@@ -1,60 +1,69 @@
 const args = process.argv.slice(2);
 const settings = require("../.env"); // settings.json
+const bcrypt = req
+
 const knex = require("knex")({
-  client: 'postgresql',
+  client: "postgresql",
   connection: {
-    host     : 'localhost',
-    user     : 'labber',
-    password : 'labber',
-    database : 'midterm',
-    port     : 5432,
+    host: "localhost",
+    user: "labber",
+    password: "labber",
+    database: "midterm",
+    port: 5432
   }
 });
 
-// module.exports = function makeDataHelpers(db) {
-//   return {
-
+module.exports = function makeDataHelpers(db) {
+  return {
 // Create User
-
+    createUser: function (email, password) {
+      return knex('users')
+      .insert({ email: email, password: bcrypt.hashSync(password, 10)})
+      .into('users')
+},
+//DataHelpers.loginUser;
+//
 
 // Find user
-  const userEmailPasswordById = (userid) => {
-    const useridInt = parseInt(userid);
-    return knex.select('email', 'password').from('users')
-      .where('id', useridInt)
-      .asCallback(function(err, rows) {
-    if (err) return console.error(err);
-    printResult(rows);
-    process.exit(0);
-    });
-  }
-    // userEmailPasswordById(args);
-
-    //         by email and password
-  const useridByEmailPassword = (email, password) => {
-    const emailStr = email.toString();
-    console.log(password)
-    const passwordStr = password.toString();
-    console.log(emailStr, passwordStr)
-    return knex.select('id').from('users')
-    .where('email', emailStr).andWhere('password', passwordStr).from('users')
+const userEmailPasswordById = userid => {
+  const useridInt = parseInt(userid);
+  return knex
+    .select("email", "password")
+    .from("users")
+    .where("id", useridInt)
     .asCallback(function(err, rows) {
       if (err) return console.error(err);
       printResult(rows);
       process.exit(0);
     });
-  }
+},
+// userEmailPasswordById(args);
 
-  // useridByEmailPassword(args[0], args[1]);
-
-
-
-  const printResult = (enter) => {
-    enter.forEach((element) => {
-      console.log(element)
+//         by email and password
+const useridByEmailPassword = (email, password) => {
+  const emailStr = email.toString();
+  console.log(password);
+  const passwordStr = password.toString();
+  console.log(emailStr, passwordStr);
+  return knex
+    .select("id")
+    .from("users")
+    .where("email", emailStr)
+    .andWhere("password", passwordStr)
+    .from("users")
+    .asCallback(function(err, rows) {
+      if (err) return console.error(err);
+      printResult(rows);
+      process.exit(0);
     });
-  }
+};
 
+// useridByEmailPassword(args[0], args[1]);
 
+const printResult = enter => {
+  enter.forEach(element => {
+    console.log(element);
+  });
+};
 
 // };
