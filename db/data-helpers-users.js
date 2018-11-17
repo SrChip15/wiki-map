f15600cce1ef259098038f87f715ccf92073b689
aaron <aaron_lai@hotmail.com>
2018-11-17 15:09:42 +0000

    commiting to merge

--- db/data-helpers-users.js
+++ db/data-helpers-users.js
@@ -12,68 +12,68 @@ const knex = require("knex")({
   }
 });
 
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
-module.exports = function makeUserDataHelpers(knex) {
-  return {
+// module.exports = function makeUserDataHelpers(knex) {
+//   return {
 
-// Create User & hash the password
-    //bcrypt.hashSync(password,10)}
-    createUser: (email, password, callback) => {
-      return knex('users')
-        .insert({ 'email': email, 'password': password }) 
-      .asCallback((err, res) => {
-        if (err) {
-          callback(err);
-          // process.exit(0); //quit the service
-        } else {
-          callback(null, res);
-          // process.exit(0);
-        }
-      });
-    },
+// // Create User & hash the password
+//     //bcrypt.hashSync(password,10)}
+//     createUser: (email, password, callback) => {
+//       return knex('users')
+//         .insert({ 'email': email, 'password': password })
+//       .asCallback((err, res) => {
+//         if (err) {
+//           callback(err);
+//           // process.exit(0); //quit the service
+//         } else {
+//           callback(null, res);
+//           // process.exit(0);
+//         }
+//       });
+//     },
 
 
-    //login the user
-    loginUser: (email,password,callback)=> {
-			knex('users').where('email',email).select(['id','name','password'])
-				.asCallback(function(err,res) { 
-					if (!res) {
-						callback(err);
-					} else if (!bcrypt.compareSync(password,res[0].password)) {
-						callback(err);
-					}
-					else if (bcrypt.compareSync(password,res[0].password)){ 
-						console.log(res[0].id);
-						callback(err,{
-							 id: res[0].id,
-							 name: res[0].name,
-							 email: res[0].email
-						}); }
-			})
-		},
+//     //login the user
+//     loginUser: (email,password,callback)=> {
+// 			knex('users').where('email',email).select(['id','name','password'])
+// 				.asCallback(function(err,res) {
+// 					if (!res) {
+// 						callback(err);
+// 					} else if (!bcrypt.compareSync(password,res[0].password)) {
+// 						callback(err);
+// 					}
+// 					else if (bcrypt.compareSync(password,res[0].password)){
+// 						console.log(res[0].id);
+// 						callback(err,{
+// 							 id: res[0].id,
+// 							 name: res[0].name,
+// 							 email: res[0].email
+// 						}); }
+// 			})
+// 		},
 
-// Find user by email and password
-    //how to match the 
-    userEmailPasswordById: (userid) => {
-      const useridInt = parseInt(userid);
-      return knex.select("email", "password")
-      .from("users")
-      .where("id", useridInt)
-      .asCallback((err, res) => {
-        if (err) {
-          callback(err);
-          // process.exit(0);
-        } else {
-          callback(null, res);
-          // process.exit(0);
-        }
-      });
-    },
+// // Find user by email and password
+//     //how to match the
+//     userEmailPasswordById: (userid) => {
+//       const useridInt = parseInt(userid);
+//       return knex.select("email", "password")
+//       .from("users")
+//       .where("id", useridInt)
+//       .asCallback((err, res) => {
+//         if (err) {
+//           callback(err);
+//           // process.exit(0);
+//         } else {
+//           callback(null, res);
+//           // process.exit(0);
+//         }
+//       });
+//     },
 
 
 //find user Id by email and password
-    userIdByEmailPassword: (email, password, callback) => {
+    const userIdByEmailPassword = (email, password, callback) => {
       const emailStr = email.toString();
       const passwordStr = password.toString();
       return knex.select('id').from('users')
@@ -83,11 +83,15 @@ module.exports = function makeUserDataHelpers(knex) {
       .asCallback((err, res) => {
         if (err) {
           callback(err);
-          // process.exit(0);
+          process.exit(0);
         } else {
           callback(null, res);
-          // process.exit(0);
+          process.exit(0);
         }
       });
     }
-};
+// };
+
+userIdByEmailPassword(args[0], args[1], (err, result) => {
+  console.log(result);
+})
