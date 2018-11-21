53bff6498cde41e4847075cec6d69dcedc78e9ed
Sri Nath <sri.developer731@gmail.com>
2018-11-21 02:39:50 -0500

    Add register HTML page

--- routes/register.js
+++ routes/register.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 
-module.exports = function(dataHelpers) {
-  router.get("/register", (req, res) => {
+module.exports = function (UserDataHelpers) {
+  router.get("/", (req, res) => {
     res.render("register"); // make the ejs
   });
 
   // insert the input info into database! have not done yet
   router.post("/register", (req, res) => {
     if (req.body.email && req.body.password) {
-      dataHelpers.createUser(req.body.email, req.body.password, (err, user) => {
+      UserDataHelpers.createUser(req.body.email, req.body.password, (err, user) => {
         if (err) throw err;
         console.log(user);
         // req.body.user_id = users[0]; //user the seesion later
